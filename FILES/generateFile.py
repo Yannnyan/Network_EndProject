b82d82dd29bb063aa24e9f052d9f2944303c824a
Yan <regoner2@gmail.com>
2022-02-28 09:47:48 -0500

    sample file sending with udp

---
+++ FILES/generateFile.py
@@ -0,0 +1,13 @@
+
+
+def generateFile(size: int, name: str): # size in kilobtyes, name
+    filename = name + ".txt"
+    with open(filename,"w") as file:
+        byt = 0
+        while byt <= size * 1024:
+            file.write("s")
+            byt += 1
+            if byt == 1023:
+                file.write("\n")
+
+generateFile(4,"baby_shark")
